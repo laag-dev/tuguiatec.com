515dfe800aa981ea824ec04c61b7961df6596df1
laag-dev <laag.08@gmail.com>
2024-10-25 17:46:11 -0500

    componente hero completado

--- components/ui/TextGenerateEffect.tsx
+++ components/ui/TextGenerateEffect.tsx
@@ -29,7 +29,7 @@ export const TextGenerateEffect = ({
         delay: stagger(0.2),
       }
     );
-  }, [animate, duration, filter]); // Asegúrate de incluir todas las dependencias necesarias
+  }, [filter, duration, scope]); // Asegúrate de incluir todas las dependencias necesarias
 
   const renderWords = () => {
     return (
